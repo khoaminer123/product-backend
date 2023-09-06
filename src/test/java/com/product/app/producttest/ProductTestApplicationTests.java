e07f94e2751fc4912b5f9004bd93ed0e43e38f77
Khoa <nhkhoa2101@gmail.com>
2023-09-06 21:44:24 +0700

    no message

---
+++ src/test/java/com/product/app/producttest/ProductTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.app.producttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
